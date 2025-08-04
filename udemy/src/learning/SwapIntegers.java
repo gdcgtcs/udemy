3399923a3019aa0f95f3ac96be9386bc76476ff4
Guillermo Chang <guillermo.chang.tcs@gmail.com>
2025-08-04 15:36:20 -0600

    swap integers

---
+++ udemy/src/learning/SwapIntegers.java
@@ -0,0 +1,35 @@
+package learning;
+
+public class SwapIntegers {
+	public static void main(String[] args) {
+
+		int a = 3;
+		int b = 5;
+
+		// Print the initial values
+		System.out.println("Before swapping a = " + a + ", b = " + b);
+
+		// Calling the method to swap
+		swapUsingAdditionSubtraction(a, b);
+
+	}
+
+	// Method to swap two integers using addition and subtraction
+	public static void swapUsingAdditionSubtraction(int a, int b) {
+
+		// Step 1: Add both a and b and will store the result in a
+		a = a + b;
+
+		// Step 2: Subtract the new value of a by b to get the original value of a
+		b = a - b;
+
+		// Step 3: Subtract the new value of b (Which was originally a) from a to get
+		// the original value of b
+		a = a - b;
+
+		// Display these swapped values
+		System.out.println("After swapping a = " + a + ", b = " + b);
+
+	}
+
+}
